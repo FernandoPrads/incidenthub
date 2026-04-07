7efa8bd8cdfed4ea6f35e4c21e23661f53bae5a7
FernandoPrads <fernandoprads83@gmail.com>
2026-04-07 15:31:16 -0300

    Criando projeto

---
+++ incidenthub/src/main/java/br/com/pradela/incidenthub/IncidenthubApplication.java
@@ -0,0 +1,13 @@
+package br.com.pradela.incidenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidenthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidenthubApplication.class, args);
+	}
+
+}
